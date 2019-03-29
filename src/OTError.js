24ec83a6e1fb02dc59729e7d2e34b04f1f56129a
Julien Meichelbeck <julien.meichelbeck@doctolib.fr>
2019-03-29 15:04:24 +0100

    add onError

--- src/OTError.js
+++ src/OTError.js
@@ -1,14 +1,23 @@
+const hooks = []
+
+const onError = (onError) => {
+  hooks.push(onError)
+}
+
 const handleError = (error) => {
+  hooks.forEach(hook => hook(error))
   console.log('OTRN JS: There was an error: ', error);
 };
 
 const handleSignalError = (error) => {
   if (error) {
+    hooks.forEach(hook => hook(error))
     console.log(`OTRN JS: There was an error sending the signal ${error}`);
   }
 };
 
 export {
+  onError,
   handleError,
   handleSignalError,
 };
